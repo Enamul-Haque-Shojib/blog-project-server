4ba1377f12b026e8390d0e717c02dac66db2764a
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2024-12-21 12:53:58 +0600

    admin controllers added

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -3,8 +3,8 @@ import { ErrorRequestHandler } from "express";
 import { TErrorSources } from "../interface/error";
 import { ZodError } from "zod";
 import handleZodError from "../errors/handleZodError";
-// import handleValidationError from "../errors/handleValidationError";
-// import handleCastError from "../errors/handleCastError";
+import handleValidationError from "../errors/handleValidationError";
+import handleCastError from "../errors/handleCastError";
 import handleDuplicateError from "../errors/handleDuplicateError";
 import AppError from "../errors/AppError";
 import config from "../config";
@@ -27,20 +27,20 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     error = simplifiedError?.errorSources;
 
   }
-  //  else if (err?.name === 'ValidationError') {
-  //   const simplifiedError = handleValidationError(err);
-  //   statusCode = simplifiedError?.statusCode;
-  //   message = simplifiedError?.message;
-  //   error = simplifiedError?.errorSources;
+   else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    error = simplifiedError?.errorSources;
 
-  // }
-  //  else if (err?.name === 'CastError') {
-  //   const simplifiedError = handleCastError(err);
-  //   statusCode = simplifiedError?.statusCode;
-  //   message = simplifiedError?.message;
-  //   error = simplifiedError?.errorSources;
+  }
+   else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    error = simplifiedError?.errorSources;
     
-  // } 
+  } 
   else if (err?.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
